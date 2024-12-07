a4d15726783ec508eb73eae5e1892d45b8caf5ab
Alexa314 <alexa@Alexas-MacBook-Air.local>
2024-12-07 11:33:51 -0500

    Handle CrystalColor much better

--- Source/Entities/CustomOffsetSpinner.cs
+++ Source/Entities/CustomOffsetSpinner.cs
@@ -12,9 +12,8 @@ namespace Celeste.Mod.StunningHelper;
 [TrackedAs(typeof(CrystalStaticSpinner))]
 public class CustomOffsetSpinner : CrystalStaticSpinner
 {
-public CustomOffsetSpinner(EntityData data, Vector2 offset) : base(data, offset, (data.Attr("color") == "blue" ? CrystalColor.Blue : (data.Attr("color") == "red" | data.Attr("color") == "core") ? CrystalColor.Red : data.Attr("color") == "purple" ? CrystalColor.Purple : CrystalColor.Rainbow))
+public CustomOffsetSpinner(EntityData data, Vector2 offset) : base(data, offset, (data.Attr("color")=="core" ? (CrystalColor)(-1) : data.Enum<CrystalColor>("color")))
 {
-	// I don't know a better way to do the crystal colors I hate crystal colors
 	this.offset = data.Float("offset");
 }
 }
