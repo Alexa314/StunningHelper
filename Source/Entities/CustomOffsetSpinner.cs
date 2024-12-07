b3a32e566386f6c5a4fcb3b5a9f5c3b571083bc7
Alexa314 <alexa@Alexas-MacBook-Air.local>
2024-12-06 23:28:41 -0500

    Made all spinner colors work

--- Source/Entities/CustomOffsetSpinner.cs
+++ Source/Entities/CustomOffsetSpinner.cs
@@ -1,16 +1,20 @@
 using Celeste.Mod.Entities;
 using Microsoft.Xna.Framework;
 using Monocle;
+using Celeste;
 
 namespace Celeste.Mod.StunningHelper;
+
+
 
 [CustomEntity("StunningHelper/CustomOffsetSpinner")]
 
 [TrackedAs(typeof(CrystalStaticSpinner))]
 public class CustomOffsetSpinner : CrystalStaticSpinner
 {
-public CustomOffsetSpinner(EntityData data, Vector2 offset, CrystalColor color) : base(data, offset, color)
+public CustomOffsetSpinner(EntityData data, Vector2 offset) : base(data, offset, (data.Attr("color") == "blue" ? CrystalColor.Blue : (data.Attr("color") == "red" | data.Attr("color") == "core") ? CrystalColor.Red : data.Attr("color") == "purple" ? CrystalColor.Purple : CrystalColor.Rainbow))
 {
+	// I don't know a better way to do the crystal colors I hate crystal colors
 	this.offset = data.Float("offset");
 }
 }
